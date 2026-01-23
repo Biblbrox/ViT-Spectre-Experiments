52692118307f5e1ca27b55298cbd79b8e837c8ca
Aleksey Kuchkov <biblbroxxx@gmail.com>
2026-01-23 14:56:50 +0300

    Added config-based system for export repl

--- export.py
+++ export.py
@@ -1,36 +1,43 @@
 # %% Cell 1
 from spectre_vit.base_vit import ViT
+from configs.parser import parse_config
 import torch
 # %% Cell 2
 # Model configuration
-BATCH_SIZE = 16
-EPOCHS = 1000
-LEARNING_RATE = 1e-3
-NUM_CLASSES = 100
-PATCH_SIZE = 4
-IMG_SIZE = 32
-IN_CHANNELS = 3
-NUM_HEADS = 8
-DROPOUT = 0.001
-HIDDEN_DIM = 256
-ADAM_WEIGHT_DECAY = 0.01
-ADAM_BETAS = (0.9, 0.999)
-ACTIVATION = 'gelu'
-NUM_ENCODERS = 4
-# EMBED_DIM = (PATCH_SIZE**2) * IN_CHANNELS # 16
-EMBED_DIM = 512
-NUM_PATCHES = (IMG_SIZE // PATCH_SIZE) ** 2  # 49
-USE_SPECTRE = True
-SPECTRE_THRESHOLD = 1.0
-METHOD = 'fft_mh_spectrelayers'
+config_path = "configs/spectre_vit.py"
+config = parse_config(config_path)
+random_seed = config.random_seed
+batch_size = config.batch_size
+epochs = config.epochs
+learning_rate = config.learning_rate
+num_classes = config.num_classes
+patch_size = config.patch_size
+img_size = config.img_size
+in_channels = config.in_channels
+num_heads = config.num_heads
+dropout = config.dropout
+hidden_dim = config.hidden_dim
+adam_weight_decay = config.adam_weight_decay
+adam_betas = config.adam_betas
+activation = config.activation
+num_encoders = config.num_encoders
+embed_dim = config.embed_dim
+num_patches = config.num_patches
+use_spectre = config.use_spectre
+spectre_threshold = config.spectre_threshold
+method = config.method
+experiment_name = "spectre_vit_fftmh16_spectrelayers_fusedheads"
+use_distillation = False
+
+
 # %% Cell 3
 
 
 def export_model(model_name):
-    model = ViT(img_size=IMG_SIZE, patch_size=PATCH_SIZE, in_channels=IN_CHANNELS,
-                num_classes=NUM_CLASSES, embed_dim=EMBED_DIM, num_encoders=NUM_ENCODERS,
-                num_heads=NUM_HEADS, hidden_dim=HIDDEN_DIM, dropout=DROPOUT, activation=ACTIVATION,
-                method=METHOD).cuda()
+    model = ViT(img_size=img_size, patch_size=patch_size, in_channels=in_channels,
+                num_classes=num_classes, embed_dim=embed_dim, num_encoders=num_encoders,
+                num_heads=num_heads, hidden_dim=hidden_dim, dropout=dropout, activation=activation,
+                method=method).cuda()
     print(
         f"Exporting model: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
     torch.save(model, f"export/{model_name}.pt")
@@ -39,5 +46,5 @@ def export_model(model_name):
                       "input"], output_names=["output"], dynamo=True)
 
 
-model_name = f"spectre_vit{IMG_SIZE}_HID{HIDDEN_DIM}_HEAD{NUM_HEADS}_ENC{NUM_ENCODERS}"
+model_name = f"spectre_vit{img_size}_HID{hidden_dim}_HEAD{num_heads}_ENC{num_encoders}"
 export_model(model_name)
